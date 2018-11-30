384a2626c0901ee50f707d5ad30e1550c3431049
dadjei <dadjei@DADJEI-CZ.cz.oracle.com>
2018-11-30 17:22:41 +0100

    add sort function

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
 
 class ProductList extends React.Component {
   render() {
+    const products = Seed.products.sort((a, b) => (
+        b.votes - a.votes
+        ));
     const productComponents = Seed.products.map((product) =>(
         <Product
           key={'product-' + product.id}
